cf301e2e47ce87c80d3e8e074c3f4b0769788548
syedomer17 <syedomerali2006@gmail.com>
2026-04-05 15:53:07 +0530

    data types comments and naming rules

---
+++ python/class1.py
@@ -0,0 +1,35 @@
+print("hello world")
+# This is a single line comment 
+
+""" This is a multi-line comment """
+
+# variables are kind of liek a container that holds a value. 
+
+a = 12 
+b = 56 
+
+chacha = 45 
+papa = 46 
+
+""" 
+Naming convention 
+
+1. Camel Case : This is a naming convention where the first letter of each word is capitalized, except for the first word. For example: myVariableName, userName, etc.
+
+2. Pascal Case : This is a naming convention where the first letter of each word is capitalized, including the first word. For example: MyVariableName, UserName, etc.
+
+3. Snake Case : This is a naming convention where words are separated by underscores, and all letters are lowercase. For example: my_variable_name, user_name, etc.
+
+naming rules: 
+1. variable can't start with a number 
+2. In variable you can't use space 
+3. variable name can't be a keyword (like print, input, etc.)
+4. variable name can contain letters, numbers, and underscores, but it must start with a letter or an underscore.
+"""
+
+"""
+Data types in Python:
+1. Number (int, float, complex)
+2. String (str): A sequence of characters enclosed in single quotes (' '), double quotes (" "), or triple quotes (''' ''' or """ """). For example: 'Hello', "World", '''This is a string''', etc.
+3. Boolean (bool)
+"""
