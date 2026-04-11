1e7fb048c5de4ebae0ccf386ddc67634b3b6252b
syedomer17 <syedomerali2006@gmail.com>
2026-04-11 16:29:38 +0530

    class 10

---
+++ python/class10.py
@@ -0,0 +1,22 @@
+a = 11
+
+while a < 10: 
+    print(a)
+    a += 1 
+
+
+while a >= 0: 
+    a += 1
+    if a == 10: 
+        break
+    print(a)
+
+while a <=  10: 
+    a += 1
+    if a == 5: 
+        continue
+    print(a)
+else : 
+    print("The loop is over")   
+
+  
