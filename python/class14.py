686708dbf7c6f669c83fdd68c05d23679d425680
syedomer17 <syedomerali2006@gmail.com>
2026-04-16 22:22:48 +0530

    class 14

---
+++ python/class14.py
@@ -0,0 +1,28 @@
+/*
+
+Data structures and algorithms - class 14
+
+In build data structures and algorithms, we will be covering the following topics :
+
+1. List
+2. Tuple
+3. Dictionary 
+4. set
+
+Customized Data structures:
+1. Stack
+2. Queue
+3. Linked List
+4. trees 
+5. Graphs 
+
+*/
+
+# list 
+a =  [12, 64, 78]
+
+# tuple 
+b = (12, 64, 78)
+
+# set 
+c = {12, 64, 78}
