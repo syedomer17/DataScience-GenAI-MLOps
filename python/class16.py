81359a819d8fe77a6b40a76c9be9c817c4d7f957
syedomer17 <syedomerali2006@gmail.com>
2026-04-19 16:21:58 +0530

    class 16 with problems of lis

---
+++ python/class16.py
@@ -0,0 +1,68 @@
+a = [10,20,30,40,50]
+sum = 0 
+for i in a: 
+    sum += i
+print(f"sum of the list is {sum}")
+print(f"average of the list is {sum/len(a)}")
+
+a = [1,45,23,89,45,90,12,36,82]
+max = a[0]
+index = 0 
+
+for i in range(len(a)):
+    if a[i] > max: 
+        max = a[i]
+        index = i 
+print(f"maximum element in the list is {max} and its index is {index}")
+
+a = [1,45,23,89,45,90,12,36,82]
+max = a[0]
+max2 = a[0]
+index = 0
+index2 = 0
+
+for i in range(len(a)): 
+    if a[i] > max:
+        max2 = max 
+        max = a[i]
+        index2 = index
+        index = i
+    elif a[i] > max2 and a[i] != max:
+        max2 = a[i]
+        index2 = i
+print(f"maximum element in the list is {max} and its index is {index}")
+print(f"second maximum element in the list is {max2} and its index is {index2}")
+
+a = [12,13,14,15,16,17,11,23,24,25,36,78,90]
+for i in range(len(a) - 1): 
+    if a[i] < a[i + 1]: 
+        continue
+    else: 
+        print("list is not in ascending order")
+        break
+else:
+    print("list is in ascending order")
+
+a = [10,20,30,40,50]
+
+for i in range(len(a) - 1): 
+    a[i], a[i + 1] = a[i + 1],a[i] # we are swapping the elements of the list
+print(a) # [50, 40, 30, 20, 10]
+
+for i in range(len(a) -1, 0, -1): 
+    a[i], a[i - 1] = a[i - 1], a[i] # we are swapping the elements of the list
+print(a) # [50, 20, 30, 40, 10]
+
+k = int(input("Enter the number to do rotate:"))
+
+for i in range(k): 
+    for i in range(len(a) - 1): 
+        a[i], a[i + 1] = a[i + 1], a[i] # we are swapping the elements of the list
+print(a) 
+
+a  =  [10,20,30,40,50]
+b = len(a) - 1
+for i in range(len(a) // 2): 
+    a[i], a[b] = a[b], a[i] # we are swapping the elements of the list
+    b -= 1
+print(a) # [50, 40, 30, 20, 10]
